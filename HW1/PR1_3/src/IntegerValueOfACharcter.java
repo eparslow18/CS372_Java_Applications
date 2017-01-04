f4003a17b671622026046576a77cfe988d37af43
ADMIN\eparslow18 <eparslow18@my.whitworth.edu>
2017-01-04 15:20:29 -0800

    Homework 1, project 3 Integer Value Of A Character

---
+++ HW1/PR1_3/src/IntegerValueOfACharcter.java
@@ -0,0 +1,23 @@
+
+public class IntegerValueOfACharcter {
+
+	public static void main(String[] args) {
+		System.out.printf("The integer equivant of %c is %d\n",'A', ((int)'A'));
+		System.out.printf("The integer equivant of %c is %d\n",'B', ((int)'B'));
+		System.out.printf("The integer equivant of %c is %d\n",'C', ((int)'C'));
+		System.out.printf("The integer equivant of %c is %d\n",'a', ((int)'a'));
+		System.out.printf("The integer equivant of %c is %d\n",'b', ((int)'b'));
+		System.out.printf("The integer equivant of %c is %d\n",'c', ((int)'c'));
+		System.out.printf("The integer equivant of %c is %d\n",'A', ((int)'A'));
+		
+		System.out.printf("The integer equivant of %c is %d\n",'1', ((int)'1'));
+		System.out.printf("The integer equivant of %c is %d\n",'2', ((int)'2'));
+		System.out.printf("The integer equivant of %c is %d\n",'$', ((int)'$'));
+		System.out.printf("The integer equivant of %c is %d\n",'^', ((int)'^'));
+		System.out.printf("The integer equivant of %c is %d\n",'.', ((int)'.'));
+		System.out.printf("The integer equivant of %c is %d\n",'/', ((int)'/'));
+		System.out.printf("The integer equivant of '%c' is %d\n",' ', ((int)' '));
+
+	}
+
+}
